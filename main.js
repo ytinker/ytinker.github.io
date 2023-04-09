4a68696e8f85161a77fbc96f9f329b390184d173
ytinker <ysc21@proton.me>
2023-04-08 23:31:09 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,20 +2,20 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 const loader = new GLTFLoader();
 
     // Create the scene, camera, and renderer
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer({ antialias: true });
+const scene = new Scene();
+const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // Add some lights to the scene
-const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
-const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+const ambientLight = new AmbientLight(0xffffff, 0.3);
+const directionalLight = new DirectionalLight(0xffffff, 0.5);
 directionalLight.position.set(-1, 2, 4);
 scene.add(ambientLight, directionalLight);
 
 // Load the GLB file and add it to the scene
-const loader = new THREE.GLTFLoader();
+const loader = new GLTFLoader();
 loader.load( 'miku_hatsune_rig.glb', function ( gltf ) {
 
 	scene.add( gltf.scene );
