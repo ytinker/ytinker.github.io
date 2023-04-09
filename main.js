f5a3bd5f1eb40047043840915f41b7b7fb25b1e6
ytinker <yong.choe@macefusion.com>
2023-04-08 22:12:39 -0400

    Update main.js

--- main.js
+++ main.js
@@ -20,21 +20,4 @@ loader.load( 'miku_hatsune.glb', function ( gltf ) {
 
 } );
 
-
-const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
-
-camera.position.z = 5;
-
-function animate() {
-	requestAnimationFrame( animate );
-
-	cube.rotation.x += 0.01;
-	cube.rotation.y += 0.01;
-
-	renderer.render( scene, camera );
-}
-
 animate();
