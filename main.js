4b046604471308cf100461725e9ce18d154d16c2
ytinker <ysc21@proton.me>
2023-04-09 01:36:17 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xffffff);
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
@@ -26,13 +27,13 @@ function ( error ) {
 } );
 
 // Set the camera position
-const geometry = new THREE.BoxGeometry( 2, 2, 2 );
+const geometry = new THREE.BoxGeometry( 1, 1, 1 );
 const material = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
-camera.position.z = 10;
-camera.position.y = 10;
+camera.position.z = 5;
+
 
 
 
