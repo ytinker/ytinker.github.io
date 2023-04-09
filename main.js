0e8b1fbc17f51f2889a16ebe9b4e2389d379b5d4
ytinker <ysc21@proton.me>
2023-04-08 23:27:39 -0400

    test

--- main.js
+++ main.js
@@ -1,20 +1,39 @@
-import * as THREE from 'https://cdn.jsdelivr.net/npm/three@0.151.3/build/three.min.js';
-import { GLTFLoader } from 'https://cdn.jsdelivr.net/npm/three@0.151.3/examples/jsm/loaders/GLTFLoader.js';
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
-
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize( window.innerWidth, window.innerHeight );
-document.body.appendChild( renderer.domElement );
-
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 const loader = new GLTFLoader();
 
+    // Create the scene, camera, and renderer
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+// Add some lights to the scene
+const ambientLight = new THREE.AmbientLight(0xffffff, 0.3);
+const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+directionalLight.position.set(-1, 2, 4);
+scene.add(ambientLight, directionalLight);
+
+// Load the GLB file and add it to the scene
+const loader = new THREE.GLTFLoader();
 loader.load( 'miku_hatsune_rig.glb', function ( gltf ) {
 
 	scene.add( gltf.scene );
 
 }, undefined, function ( error ) {
-	
+
 	console.error( error );
+
 } );
+
+
+    // Set the camera position
+    camera.position.set(0, 1, 4);
+
+    // Animate the scene
+    function animate() {
+      requestAnimationFrame(animate);
+      renderer.render(scene, camera);
+    }
+
+    animate();
