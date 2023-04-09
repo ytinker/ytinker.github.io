a486ea1d0095f4040e4ee18acd09019b996e548d
ytinker <ysc21@proton.me>
2023-04-08 23:37:58 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,15 +1,15 @@
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
     // Create the scene, camera, and renderer
-const scene = new Scene();
+const scene = new scene();
 const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 const renderer = new WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // Add some lights to the scene
-const ambientLight = new AmbientLight(0xffffff, 0.3);
-const directionalLight = new DirectionalLight(0xffffff, 0.5);
+const ambientLight = new ambientLight(0xffffff, 0.3);
+const directionalLight = new directionalLight(0xffffff, 0.5);
 directionalLight.position.set(-1, 2, 4);
 scene.add(ambientLight, directionalLight);
 
