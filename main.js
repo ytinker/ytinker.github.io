24a7c1c02a487d10560630cd2bcae35cd6353e4f
ytinker <ysc21@proton.me>
2023-04-09 01:43:31 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-scene.background = new THREE.Color(0x000000);
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
