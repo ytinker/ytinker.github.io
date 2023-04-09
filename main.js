b53dbde6415f62934c73654ddd1a075f06d74957
ytinker <ysc21@proton.me>
2023-04-09 01:52:17 -0400

    Update main.js

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
+renderer.gammaFactor=2.2;
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
@@ -27,11 +28,12 @@ function ( error ) {
 
 // Set the camera position
 const geometry = new THREE.BoxGeometry( 2, 2, 2);
-const material = new THREE.MeshBasicMaterial( { color: 0x0000ff } );
+const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
 camera.position.z = 5;
+camera.position.y =5;
 
 
 
@@ -39,7 +41,7 @@ camera.position.z = 5;
     // Animate the scene
 function animate() {
   requestAnimationFrame(animate);
-  renderer.gammaFactor=2.2;
+
   cube.rotation.x += 0.002;
   cube.rotation.y += 0.002;
   renderer.render(scene, camera);
