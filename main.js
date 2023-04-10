41ecaa1b07e607000372634d1eece76159fb2ee2
ytinker <ysc21@proton.me>
2023-04-09 23:13:49 -0400

    Update main.js

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ function ( error ) {
 
 // Set the camera position
 renderer.gammaFactor=2.2;
-const geometry = new THREE.BoxGeometry( 4, 3, 2);
+const geometry = new THREE.BoxGeometry( 10,10, 10);
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
