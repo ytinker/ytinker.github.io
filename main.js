3a4bb79e3616eee36aedaea7c26d9ddbb3be5b00
ytinker <ysc21@proton.me>
2023-04-10 01:01:50 -0400

    Update main.js

--- main.js
+++ main.js
@@ -2,11 +2,11 @@ import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+const camera = new THREE.PerspectiveCamera( 75, screen.width / screen.height, 0.1, 1000 );
 
 const renderer = new THREE.WebGLRenderer();
 renderer.gammaFactor=2.2;
-renderer.setSize( window.innerWidth, window.innerHeight );
+renderer.setSize( screen.width, screen.height );
 document.body.appendChild( renderer.domElement );
 
 // Set the camera position
