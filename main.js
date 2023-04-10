2e7d13c1e2588487376872c8464f65540e337d5d
ytinker <ysc21@proton.me>
2023-04-09 23:02:31 -0400

    Update main.js

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ function ( error ) {
 const geometry = new THREE.BoxGeometry( 2, 2, 2);
 const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+//scene.add( cube );
 
 camera.position.z = 5;
 camera.position.y =-2;
